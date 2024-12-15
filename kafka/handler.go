af51c6d688100bffa1d20359913e2085d73dc23a
Ddarli <ivanlatyshev024@gmail.com>
2024-12-15 16:22:16 +0300

    handler

---
+++ kafka/handler.go
@@ -0,0 +1,35 @@
+package kafka
+
+import (
+	"github.com/IBM/sarama"
+)
+
+type EventsHandler struct {
+	eventsKafka chan *sarama.ConsumerMessage
+}
+
+func NewDefaultHandler(
+	eventsKafka chan *sarama.ConsumerMessage,
+) *EventsHandler {
+	return &EventsHandler{eventsKafka: eventsKafka}
+}
+
+func (kc *EventsHandler) Setup(_ sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (kc *EventsHandler) Cleanup(session sarama.ConsumerGroupSession) error {
+	session.Commit()
+
+	return nil
+}
+
+func (kc *EventsHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) (err error) {
+	for message := range claim.Messages() {
+		kc.eventsKafka <- message
+
+		session.MarkMessage(message, "")
+	}
+
+	return err
+}
